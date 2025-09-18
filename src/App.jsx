b9cbec524f7b400e8f9056d538a5e4aacb5458dd
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-09-18 21:15:21 +0600

    react project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Countries from './countries/countries'
+function App() {
+ 
+  return (
+    <>
+     
+      <h1> React</h1>
+    <Countries></Countries>
+      
+    </>
+  )
+}
+
+export default App
